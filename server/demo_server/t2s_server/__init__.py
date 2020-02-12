fe2e720c75cac83c2b44424e66e080925f3213f1
rinjac <robert.injac@ondewo.com>
2020-02-12 17:35:56 +0100

    Further progress on demo server.

--- server/demo_server/t2s_server/__init__.py
+++ server/demo_server/t2s_server/__init__.py
@@ -10,7 +10,7 @@ import nemo_tts
 t2s_server = Flask(__name__)
 t2s_server.logger.setLevel(logging.DEBUG)
 
-WORK_DIR = "/opt/natalia/server/demo_server/s2t_server/tmp"
+WORK_DIR = "/opt/stella/server/demo_server/t2s_server/tmp/"
 
 neural_factory = nemo.core.NeuralModuleFactory(
     placement=nemo.core.DeviceType.GPU,
@@ -19,11 +19,13 @@ neural_factory = nemo.core.NeuralModuleFactory(
 # =============================== Waveglow ===============================
 WAVEGLOW_YAML: str = "/opt/stella/models/waveglow/waveglow.yaml"
 WAVEGLOW_SIGMA: float = 0.6
+WAVEGLOW: str = "/opt/stella/models/waveglow/WaveGlowNM.pt"
 
 yaml = YAML(typ="safe")
 with open(WAVEGLOW_YAML) as file:
     waveglow_params = yaml.load(file)
 waveglow = nemo_tts.WaveGlowInferNM(sigma=WAVEGLOW_SIGMA, **waveglow_params["WaveGlowNM"])
+waveglow.restore_from(WAVEGLOW)
 
 # =============================== Tacotron2 English model =============================== 
 MODEL_YAML_EN: str = "/opt/stella/models/tacotron2/en/tacotron2.yaml"
@@ -41,14 +43,18 @@ data_preprocessor = nemo_asr.AudioToMelSpectrogramPreprocessor(
 text_embedding = nemo_tts.TextEmbedding(
     len(tacotron2_params["labels"]) + 3, **tacotron2_params["TextEmbedding"],  # + 3 special chars
 )
+text_embedding.restore_from(TACOTRON2_EMBEDDING_EN)
 t2_enc = nemo_tts.Tacotron2Encoder(**tacotron2_params["Tacotron2Encoder"])
+t2_enc.restore_from(TACOTRON2_ENCODER_EN)
 t2_dec = nemo_tts.Tacotron2DecoderInfer(**tacotron2_params["Tacotron2Decoder"])
+t2_dec.restore_from(TACOTRON2_DECODER_EN)
 t2_postnet = nemo_tts.Tacotron2Postnet(**tacotron2_params["Tacotron2Postnet"])
+t2_postnet.restore_from(TACOTRON2_POSTNET_EN)
 t2_loss = nemo_tts.Tacotron2Loss(**tacotron2_params["Tacotron2Loss"])
 makegatetarget = nemo_tts.MakeGate()
 total_weights = text_embedding.num_weights + t2_enc.num_weights + t2_dec.num_weights + t2_postnet.num_weights
 
-# =============================== German model =============================== 
+# ===============================
 
 from t2s_server import routes
 if __name__ == '__main__':
---
+++ server/demo_server/t2s_server/routes.py
@@ -0,0 +1,118 @@
+import json
+import os
+import time
+import re
+import wave
+import logging
+import librosa
+
+from flask import request, send_from_directory
+from werkzeug.utils import secure_filename
+from ruamel.yaml import YAML
+from scipy.io.wavfile import write
+
+import nemo_asr
+import nemo_tts
+from t2s_server import neural_factory, tacotron2_params, text_embedding, t2_enc, t2_dec, t2_postnet, t2_loss, \
+    waveglow, t2s_server, waveglow_params, WORK_DIR
+
+
+def get_audio(sample_path):
+
+    # make graph
+    data_layer = nemo_asr.TranscriptDataLayer(
+        path=sample_path,
+        labels=tacotron2_params['labels'],
+        batch_size=1,
+        num_workers=1,
+        bos_id=len(tacotron2_params['labels']),
+        eos_id=len(tacotron2_params['labels']) + 1,
+        pad_id=len(tacotron2_params['labels']) + 2,
+        shuffle=False,
+    )
+    transcript, transcript_len = data_layer()
+    transcript_embedded = text_embedding(char_phone=transcript)
+    transcript_encoded = t2_enc(char_phone_embeddings=transcript_embedded, embedding_length=transcript_len,)
+    mel_decoder, gate, alignments, mel_len = t2_dec(
+        char_phone_encoded=transcript_encoded, encoded_length=transcript_len,
+    )
+    mel_postnet = t2_postnet(mel_input=mel_decoder)
+
+    infer_tensors = [mel_postnet, gate, alignments, mel_len]
+
+    logging.info("Running Tacotron 2")
+    # Run tacotron 2
+    evaluated_tensors = neural_factory.infer(tensors=infer_tensors, offload_to_cpu=False)
+    mel_len = evaluated_tensors[-1]
+    logging.info("Done Running Tacotron 2")
+    filterbank = librosa.filters.mel(
+        sr=tacotron2_params["sample_rate"],
+        n_fft=tacotron2_params["n_fft"],
+        n_mels=tacotron2_params["n_mels"],
+        fmax=tacotron2_params["fmax"],
+    )
+
+    (mel_pred, _, _, _) = infer_tensors
+    # Run waveglow
+    logging.info("Running Waveglow")
+    audio_pred = waveglow(mel_spectrogram=mel_pred)
+    evaluated_tensors = neural_factory.infer(tensors=[audio_pred])
+    logging.info("Done Running Waveglow")
+
+    #if args.waveglow_denoiser_strength > 0:
+    #    logging.info("Setup denoiser")
+    #    waveglow.setup_denoiser()
+
+    logging.info("Saving results to disk")
+    #sample = evaluated_tensors[0][0][0]
+    
+    audio = evaluated_tensors[0][0].cpu().numpy()
+    sample = audio[0]
+    sample_len = mel_len[0][0] * tacotron2_params["n_stride"]
+    sample = sample[:sample_len]
+    save_file = WORK_DIR + "/tmp.wav"
+    print(type(sample))
+    write(save_file, waveglow_params["sample_rate"], sample)
+
+    return save_file
+
+RESULT: str = """
+<audio controls>
+    <source src="tmp/tmp.wav" type="audio/wav">
+  Your browser does not support the audio element.
+</audio>
+"""
+
+@t2s_server.route('/text2speech', methods=['POST'])
+def text_2_speech():
+    if request.method == 'POST':
+ #       try:
+        text = request.form['text']
+        lang = request.form['language']
+
+        text_json = {"audio_filepath": "", "duration": 1.0, "text": text}
+        sample_path = WORK_DIR + "tmp.json"
+        with open(sample_path, 'w') as json_file:
+            json_file.write(json.dumps(text_json))
+
+        start_t = time.time()
+        save_file = get_audio(sample_path)
+        total_t = time.time() - start_t
+
+        return RESULT
+        #send_file(
+        #    save_file, 
+        #    mimetype="audio/wav")
+        #    #as_attachment=True, 
+        #    #attachment_filename="demo.wav"
+        #) 
+  #      except Exception as e:
+   #         return str(e)
+
+@t2s_server.route('/tmp/<path:filepath>')
+def data(filepath):
+    return send_from_directory('tmp', filepath)
+
+@t2s_server.route('/audiofile')
+def audiofile():
+    return t2s_server.send_static_file('audiofile.html')
