455221ff372d8b2d1a9235ca6a55d2d779f04915
rinjac <robert.injac@ondewo.com>
2020-06-19 12:10:41 +0200

    Separated inference into an inference folder, some repo reorganization, config loading

--- batch_server/routes.py
+++ batch_server/routes.py
@@ -1,71 +1,8 @@
 import json
 import time
-import logging
 from flask import request, send_file
 from scipy.io.wavfile import write
-import nemo.collections.asr as nemo_asr
-
-# for some reason Flask insists that this import must look like this
-# othwise the routes do not get imported into the server
-# so ignore the error you get in PyCharm for the following line
-from t2s_server import server
-
-from server.t2s_server import neural_factory, waveglow, waveglow_params, WORK_DIR, models
-
-
-def get_audio(sample_path, lang):
-
-    # make graph
-    data_layer = nemo_asr.TranscriptDataLayer(
-        path=sample_path,
-        labels=models[lang]['params']['labels'],
-        batch_size=1,
-        num_workers=1,
-        bos_id=len(models[lang]['params']['labels']),
-        eos_id=len(models[lang]['params']['labels']) + 1,
-        pad_id=len(models[lang]['params']['labels']) + 2,
-        shuffle=False,
-    )
-    transcript, transcript_len = data_layer()
-    transcript_embedded = models[lang]['embedding'](char_phone=transcript)
-    transcript_encoded = models[lang]['encoder'](char_phone_embeddings=transcript_embedded, embedding_length=transcript_len,)
-    mel_decoder, gate, alignments, mel_len =  models[lang]['decoder'](
-        char_phone_encoded=transcript_encoded, encoded_length=transcript_len,
-    )
-    mel_postnet = models[lang]['postnet'](mel_input=mel_decoder)
-
-    infer_tensors = [mel_postnet, gate, alignments, mel_len]
-
-    logging.info("Running Tacotron 2")
-    # Run tacotron 2
-    evaluated_tensors = neural_factory.infer(tensors=infer_tensors, offload_to_cpu=False)
-    mel_len = evaluated_tensors[-1]
-    logging.info("Done Running Tacotron 2")
-
-    (mel_pred, _, _, _) = infer_tensors
-    # Run waveglow
-    logging.info("Running Waveglow")
-    audio_pred = waveglow(mel_spectrogram=mel_pred)
-    evaluated_tensors = neural_factory.infer(tensors=[audio_pred])
-    logging.info("Done Running Waveglow")
-
-    #if args.waveglow_denoiser_strength > 0:
-    #    logging.info("Setup denoiser")
-    #    waveglow.setup_denoiser()
-
-    logging.info("Saving results to disk")
-    #sample = evaluated_tensors[0][0][0]
-    
-    audio = evaluated_tensors[0][0].cpu().numpy()
-    sample = audio[0]
-    sample_len = mel_len[0][0] * models[lang]['params']["n_stride"]
-    sample = sample[:sample_len]
-    save_file = WORK_DIR + "/tmp.wav"
-    print(type(sample))
-    write(save_file, waveglow_params["sample_rate"], sample)
-
-    return save_file
-
+from . import server, WORK_DIR, nemo_inference
 
 RESULT: str = """
 <html lang="en">
@@ -124,8 +61,12 @@ def text_2_speech():
             json_file.write(json.dumps(text_json))
 
         start_t = time.time()
-        save_file = get_audio(sample_path, lang=lang)
+        sample = nemo_inference.synthesize(sample_path)
         total_t = time.time() - start_t
+
+        save_file = WORK_DIR + "/tmp.wav"
+        print(type(sample))
+        write(save_file, 22050, sample)
 
         return RESULT.format(total_t)
         #send_file(
---
+++ inference/nemo_inference.py
@@ -0,0 +1,61 @@
+import nemo
+import nemo.collections.tts as nemo_tts
+import nemo.collections.asr as nemo_asr
+import numpy as np
+from inference.load_config import load_config_nemo
+import logging
+
+
+class NemoInference:
+
+    def __init__(self, config: str, logger: logging.Logger = None):
+
+        self.neural_factory = nemo.core.NeuralModuleFactory(
+            placement=nemo.core.DeviceType.GPU,
+            backend=nemo.core.Backend.PyTorch)
+        self.config = load_config_nemo(config)
+        self.logger = logger
+
+    def synthesize(self, sample_path: str) -> np.ndarray:
+
+        # make graph
+        data_layer = nemo_asr.TranscriptDataLayer(
+            path=sample_path,
+            labels=self.config['tacotron2']['config']['labels'],
+            batch_size=1,
+            num_workers=1,
+            bos_id=len(self.config['tacotron2']['config']),
+            eos_id=len(self.config['tacotron2']['config']) + 1,
+            pad_id=len(self.config['tacotron2']['config']) + 2,
+            shuffle=False,
+        )
+        transcript, transcript_len = data_layer()
+        transcript_embedded = self.config['tacotron2']['embedding'](char_phone=transcript)
+        transcript_encoded = self.config['tacotron2']['encoder'](char_phone_embeddings=transcript_embedded,
+                                                     embedding_length=transcript_len, )
+        mel_decoder, gate, alignments, mel_len = self.config['tacotron2']['decoder'](
+            char_phone_encoded=transcript_encoded, encoded_length=transcript_len,
+        )
+        mel_postnet = self.config['tacotron2']['postnet'](mel_input=mel_decoder)
+        infer_tensors = [mel_postnet, gate, alignments, mel_len]
+
+        self.logger.info("Running Tacotron 2")
+        # Run tacotron 2
+        evaluated_tensors = self.neural_factory.infer(tensors=infer_tensors, offload_to_cpu=False)
+        mel_len = evaluated_tensors[-1]
+        self.logger.info("Done Running Tacotron 2")
+
+        (mel_pred, _, _, _) = infer_tensors
+        # Run waveglow
+        self.logger.info("Running Waveglow")
+        audio_pred = self.config['waveglow']['model'](mel_spectrogram=mel_pred)
+        evaluated_tensors = self.neural_factory.infer(tensors=[audio_pred])
+        self.logger.info("Done Running Waveglow")
+
+        # if args.waveglow_denoiser_strength > 0:
+        #    logging.info("Setup denoiser")
+        #    waveglow.setup_denoiser()
+
+        sample_len = mel_len[0][0] * self.config['tacotron2']['config']["n_stride"]
+        sample = evaluated_tensors[0][0].cpu().numpy()[0]
+        return sample[:sample_len]
