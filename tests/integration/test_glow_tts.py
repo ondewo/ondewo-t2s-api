fix pipeline, fix batch generation, introduce lowercase into normalization

tests/integration/test_glow_tts.py
@@ -4,7 +4,7 @@ from typing import Dict, Any, List
 import numpy as np
 from ruamel.yaml import YAML
 
-from inference.text2mel.constants import BATCH_INFERENCE
+from inference.text2mel.constants import BATCH_SIZE
 from inference.text2mel.glow_tts import GlowTts
 
 yaml = YAML(typ="safe")
@@ -19,6 +19,8 @@ class TestGlowTts:
 
     @staticmethod
     def test_glow_tts_text_mel_no_batch() -> None:
+        batch_config = deepcopy(test_config)
+        batch_config[BATCH_SIZE] = 1
         generator: GlowTts = GlowTts(config=test_config)
         mels: List[np.array] = generator.text2mel(['alles klar', 'noch ein text', 'und noch ein text'])
         assert len(mels) == 3
@@ -27,7 +29,7 @@ class TestGlowTts:
     @staticmethod
     def test_glow_tts_text_mel_batch() -> None:
         batch_config = deepcopy(test_config)
-        batch_config[BATCH_INFERENCE] = True
+        batch_config[BATCH_SIZE] = 2
         generator = GlowTts(config=batch_config)
         mels: List[np.array] = generator.text2mel(['alles klar', 'noch ein text', 'und noch ein text'])
         assert len(mels) == 3