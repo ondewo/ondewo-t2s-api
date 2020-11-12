c29abd90da019cc0e1bcc007159bbdffc3d7c958
Urban Tanko <urban.tanko@ondewo.com>
2020-11-12 16:39:31 +0100

    added sample generating script

---
+++ evaluation/scripts/generate_samples.py
@@ -0,0 +1,81 @@
+import os
+from typing import List, Dict, Any, Callable
+import argparse
+import multiprocessing
+from pathlib import Path
+from ruamel.yaml import YAML
+import numpy as np
+
+from scipy.io.wavfile import write as audio_write
+
+
+def gen_samples(config_path: str, sentences: List[str], output_dir: Path) -> None:
+    from inference.inference_interface import Inference
+    from inference.inference_factory import InferenceFactory
+    from normalization.pipeline_constructor import NormalizerPipeline
+    from normalization.postprocessor import Postprocessor
+    import tensorflow as tf
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+
+    yaml = YAML(typ="safe")
+    with open(config_path) as f:
+        config: Dict[str, Any] = yaml.load(f)
+
+    inference: Inference = InferenceFactory.get_inference(config['inference'])
+
+    preprocess_pipeline: NormalizerPipeline = NormalizerPipeline(config=config['normalization'])
+    postprocessor = Postprocessor()
+
+    for i, sentence in enumerate(sentences):
+        # infer
+        texts: List[str] = preprocess_pipeline.apply([sentence])
+        audio_list: List[np.ndarray] = inference.synthesize(texts=texts)
+        audio = postprocessor.postprocess(audio_list)
+
+        # convert and save wav
+        # conversion to 16-bit PCM
+        audio *= 32768
+        audio = audio.astype("int16")
+
+        # save audio to file
+        filepath = output_dir / f"sample_{i}.wav"
+        audio_write(str(filepath), 22050, audio)
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Script for creating samples of wav")
+    parser.add_argument("--config_dir", type=str, default="evaluation/configs",
+                        help="Path to folder with configs of model setups")
+    parser.add_argument("--text_samples_path", type=str, default="evaluation/sentences.txt",
+                        help="Path of the txt file with text samples (sentences)")
+    parser.add_argument("--n_samples", type=int, default=10,
+                        help="How many samples to generate per model setup, set to < 1 to use all")
+    parser.add_argument("--output_dir", type=str, default="samples",
+                        help="Path to folder where to output the samples")
+    args = parser.parse_args()
+
+    sentences: List[str] = []
+    with open(args.text_samples_path) as f:
+        sentences = [line.rstrip() for line in f]
+        sentences = sentences[:args.n_samples] if args.n_samples > 0 else sentences
+
+    # Go through all configs in config_folder and generate their samples
+    for config_name in os.listdir(args.config_dir):
+        config_path = Path(args.config_dir) / config_name
+        out_dir = Path(args.output_dir) / config_path.stem
+        os.makedirs(out_dir, exist_ok=True)
+        # using multiprocessing so that GPU memory gets released after each run
+        proc = multiprocessing.Process(target=gen_samples, args=(config_path, sentences, out_dir))
+        proc.start()
+        proc.join()
+
+    # Write the used sentences to file
+    with open(Path(args.output_dir) / "sentences.txt", "w") as f:
+        for sentence in sentences:
+            f.write(sentence + "\n")
+
+
+if __name__ == "__main__":
+    main()
