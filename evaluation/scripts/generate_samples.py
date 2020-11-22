46ab8d13852fc5c82005e31fb3a2b06be948e117
Urban Tanko <urban.tanko@ondewo.com>
2020-11-22 19:37:48 +0100

    slightly improved postprocessing

--- evaluation/scripts/generate_samples.py
+++ evaluation/scripts/generate_samples.py
@@ -9,7 +9,7 @@ import numpy as np
 from scipy.io.wavfile import write as audio_write
 
 
-def gen_samples(config_path: str, sentences: List[str], output_dir: Path) -> None:
+def gen_samples(config: Dict[str, Any], sentences: List[str], output_dir: Path) -> None:
     from inference.inference_interface import Inference
     from inference.inference_factory import InferenceFactory
     from normalization.pipeline_constructor import NormalizerPipeline
@@ -18,10 +18,6 @@ def gen_samples(config_path: str, sentences: List[str], output_dir: Path) -> Non
     gpus = tf.config.experimental.list_physical_devices('GPU')
     for gpu in gpus:
         tf.config.experimental.set_memory_growth(gpu, True)
-
-    yaml = YAML(typ="safe")
-    with open(config_path) as f:
-        config: Dict[str, Any] = yaml.load(f)
 
     inference: Inference = InferenceFactory.get_inference(config['inference'])
 
@@ -56,20 +52,38 @@ def main() -> None:
                         help="Path to folder where to output the samples")
     args = parser.parse_args()
 
+    # read sentences from a txt file
     sentences: List[str] = []
     with open(args.text_samples_path) as f:
         sentences = [line.rstrip() for line in f]
         sentences = sentences[:args.n_samples] if args.n_samples > 0 else sentences
 
+    yaml = YAML(typ="safe")
     # Go through all configs in config_folder and generate their samples
     for config_name in os.listdir(args.config_dir):
         config_path = Path(args.config_dir) / config_name
         out_dir = Path(args.output_dir) / config_path.stem
         os.makedirs(out_dir, exist_ok=True)
+
+        # load config
+        with open(config_path) as f:
+            config: Dict[str, Any] = yaml.load(f)
+            #config["inference"]["composite_inference"]["mel2audio"]["mb_melgan_tf"]["model_path"] = "/home/utanko/train.mb_melgan.germans/checkpoints/generator-300000.h5"
+
         # using multiprocessing so that GPU memory gets released after each run
-        proc = multiprocessing.Process(target=gen_samples, args=(config_path, sentences, out_dir))
+        proc = multiprocessing.Process(target=gen_samples, args=(config, sentences, out_dir))
         proc.start()
         proc.join()
+
+        # this part is used for evaluation of different mb_melgan generator (at different steps)
+        # for steps in [500_000, 760_000, 1_000_000, 1_220_000, 1_360_000, 1_500_000, 1_760_000, 2_000_000]:
+        #     config["inference"]["composite_inference"]["mel2audio"]["mb_melgan_tf"]["model_path"] = f"/home/utanko/train.mb_melgan.libritts/checkpoints/generator-{steps}.h5"
+        #     out_dir_steps = out_dir / str(steps)
+        #     os.makedirs(out_dir_steps, exist_ok=True)
+        #     # using multiprocessing so that GPU memory gets released after each run
+        #     proc = multiprocessing.Process(target=gen_samples, args=(config, sentences, out_dir_steps))
+        #     proc.start()
+        #     proc.join()
 
     # Write the used sentences to file
     with open(Path(args.output_dir) / "sentences.txt", "w") as f:
--- normalization/postprocessor.py
+++ normalization/postprocessor.py
@@ -1,13 +1,27 @@
 from typing import List
 import numpy as np
+from scipy.signal import tukey
 
 
 class Postprocessor():
-    silence = np.zeros((5000,))
+    sampling_rate = 22_050
+
+    silence_secs = 0.25  # set to 1.0 for debug
+    silence = np.zeros((int(silence_secs * sampling_rate),))
+
+    apodization_secs = 0.5
+    apodization_steps = int(apodization_secs * sampling_rate)
 
     @classmethod
     def postprocess(cls, audio_list: List[np.ndarray]) -> np.ndarray:
-        audio: np.ndarray = np.zeros((10000,))
-        for audio_part in audio_list:
+        # join audio_list with silence_secs of silence inbetween clips
+        audio: np.ndarray = audio_list[0]
+        for audio_part in audio_list[1:]:
             audio = np.concatenate((audio, cls.silence, audio_part))
-        return audio
+
+        # apodize audio at the start and end for smooth transitions (remove potential pop from beginning of audio)
+        apodization_percent = cls.apodization_steps * 2 / len(audio)
+        apodization_filter = tukey(len(audio), alpha=apodization_percent)
+        apodized_audio = audio * apodization_filter
+
+        return apodized_audio
