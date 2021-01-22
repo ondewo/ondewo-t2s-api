17f437ea8d894673a3b4058d39fc575cdbfabdda
Andrey Ryskin <andrey.ryskin@ondewo.com>
2021-01-22 09:53:09 +0100

    fix texts, move splitter test to a new file

--- tests/test_normalize_text_de.py
+++ tests/test_normalize_text_de.py
@@ -1,11 +1,8 @@
-import re
 from datetime import time, date
-from typing import List
 
 import pytest
 
 from normalization.text_preprocessing_de import TextNormalizerDe
-from normalization.text_splitter import TextSplitter
 
 normalizer = TextNormalizerDe()
 
@@ -61,9 +58,9 @@ class TestNormalization:
         ('ggg 1456lkkkk', 'ggg eins vier fünf sechs lkkkk'),
     ])
     def test_normalize_numbers(number: str, expected_result: str) -> None:
-        number_text = normalizer.normalize_numbers([number])
-        assert isinstance(number_text, list)
-        assert number_text == [expected_result]
+        number_text = normalizer.normalize_numbers(number)
+        assert isinstance(number_text, str)
+        assert number_text == expected_result
 
     @staticmethod
     @pytest.mark.parametrize('date, expected_result', [
@@ -72,7 +69,7 @@ class TestNormalization:
          'Was hast du am dritten April neunzehnhundertneunundachtzig gemacht?')
     ])
     def test_normalize_dates(date: str, expected_result: str) -> None:
-        normalized_text_with_dates: str = normalizer.normalize_single_date(date)
+        normalized_text_with_dates: str = normalizer.normalize_dates(date)
         assert isinstance(normalized_text_with_dates, str)
         assert normalized_text_with_dates == expected_result
 
@@ -102,8 +99,8 @@ class TestNormalization:
         ('sie sind am 15. Januar 1998 geboren',
          'sie sind am fünfzehnten Januar neunzehnhundertachtundneunzig geboren'),
         ("Ihre Sozialversicherungsnummer ist 1234 und sie sind am 15. Jänner 1998 geboren. Richtig?",
-            'Ihre Sozialversicherungsnummer ist eins zwei drei vier und sie sind am fünfzehnten Januar '
-            'neunzehnhundertachtundneunzig geboren. Richtig?'
+         'Ihre Sozialversicherungsnummer ist eins zwei drei vier und sie sind am fünfzehnten Januar '
+         'neunzehnhundertachtundneunzig geboren. Richtig?'
          ),
         ("Wie geht's dir???", "Wie geht's dir???")
     ]
@@ -128,58 +125,6 @@ class TestNormalization:
         normalized_text_with_time: str = normalizer.normalize_time(time)
         assert isinstance(normalized_text_with_time, str)
         assert normalized_text_with_time == expected_result
-
-    @staticmethod
-    def test_split_on_words() -> None:
-        text = 'text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11 text12 text13' \
-               ' text14 text15 text16 text17 text18 text19 text20 text21 text22 text23'
-        split_text: List[str] = TextSplitter.split_on_words(text)
-        assert isinstance(split_text, list)
-        assert len(split_text) > 1
-        assert all(map(lambda x: len(x) < 100, split_text))
-        assert sum(map(lambda x: len(x.split()), split_text)) == len(text.split())
-        assert sum(map(len, split_text)) == len(text) + 1
-
-    @staticmethod
-    @pytest.mark.parametrize('text, expected_result', [
-        ('text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11 text12 text: text.',
-         ['text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11 text12 text:', ' text.']),
-        ('text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11 text12 text; text',
-         ['text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11 text12 text;', ' text.']),
-        ('text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11 text12 text; text?',
-         ['text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11 text12 text;', ' text?']),
-        ('text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11 text12 text; text!',
-         ['text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11 text12 text;', ' text!']),
-        ('text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11 text12 text/text!',
-         ['text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11 text12 text/', 'text!']),
-    ])
-    def test_split_sentence(text: str, expected_result: str) -> None:
-        split_sentence: List[str] = TextSplitter.split_sentence(text)
-        assert isinstance(split_sentence, list)
-        assert all(map(lambda x: len(x) < 100, split_sentence))
-        assert sum(map(lambda x: len(x.split()), split_sentence)) == len(
-            list(filter(None, re.split(r'[:; |/\\]', text))))
-        assert abs(sum(map(len, split_sentence)) - len(text)) <= 1
-        assert split_sentence == expected_result
-
-    @staticmethod
-    @pytest.mark.parametrize('text, expected_result', [
-        ('text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11. text12 text: text.',
-         ['text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11.', ' text12 text: text.']),
-        ('text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11. text12 text text text text '
-         'text text text text text text text text text text text: text text  text  text.',
-         ['text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11.',
-          ' text12 text text text text text text text text text text text text text text text:',
-          ' text text  text  text.']),
-    ])
-    def test_split_text(text: str, expected_result: str) -> None:
-        split_text: List[str] = TextSplitter.split_texts([text], max_len=100)
-        assert isinstance(split_text, list)
-        assert all(map(lambda x: len(x) < 100, split_text))
-        assert sum(map(lambda x: len(x.split()), split_text)) == len(
-            list(filter(None, re.split(r'[:; |/\\]', text))))
-        assert abs(sum(map(len, split_text)) - len(text)) <= 1
-        assert split_text == expected_result
 
     @staticmethod
     @pytest.mark.parametrize('text, expected_result', [
---
+++ tests/test_text_splitter.py
@@ -0,0 +1,60 @@
+import re
+from typing import List
+
+import pytest
+
+from normalization.text_splitter import TextSplitter
+
+
+class TestTextSplitter:
+    @staticmethod
+    def test_split_on_words() -> None:
+        text = 'text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11 text12 text13' \
+               ' text14 text15 text16 text17 text18 text19 text20 text21 text22 text23'
+        split_text: List[str] = TextSplitter.split_on_words(text)
+        assert isinstance(split_text, list)
+        assert len(split_text) > 1
+        assert all(map(lambda x: len(x) < 100, split_text))
+        assert sum(map(lambda x: len(x.split()), split_text)) == len(text.split())
+        assert sum(map(len, split_text)) == len(text) + 1
+
+    @staticmethod
+    @pytest.mark.parametrize('text, expected_result', [
+        ('text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11 text12 text: text.',
+         ['text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11 text12 text:', ' text.']),
+        ('text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11 text12 text; text',
+         ['text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11 text12 text;', ' text.']),
+        ('text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11 text12 text; text?',
+         ['text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11 text12 text;', ' text?']),
+        ('text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11 text12 text; text!',
+         ['text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11 text12 text;', ' text!']),
+        ('text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11 text12 text/text!',
+         ['text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11 text12 text/', 'text!']),
+    ])
+    def test_split_sentence(text: str, expected_result: str) -> None:
+        split_sentence: List[str] = TextSplitter.split_sentence(text)
+        assert isinstance(split_sentence, list)
+        assert all(map(lambda x: len(x) < 100, split_sentence))
+        assert sum(map(lambda x: len(x.split()), split_sentence)) == len(
+            list(filter(None, re.split(r'[:; |/\\]', text))))
+        assert abs(sum(map(len, split_sentence)) - len(text)) <= 1
+        assert split_sentence == expected_result
+
+    @staticmethod
+    @pytest.mark.parametrize('text, expected_result', [
+        ('text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11. text12 text: text.',
+         ['text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11.', ' text12 text: text.']),
+        ('text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11. text12 text text text text '
+         'text text text text text text text text text text text: text text  text  text.',
+         ['text1 text2 text3 text4 text5 text6 text7 text8 text9 text10 text11.',
+          ' text12 text text text text text text text text text text text text text text text:',
+          ' text text  text  text.']),
+    ])
+    def test_split_text(text: str, expected_result: str) -> None:
+        split_text: List[str] = TextSplitter.split_texts([text], max_len=100)
+        assert isinstance(split_text, list)
+        assert all(map(lambda x: len(x) < 100, split_text))
+        assert sum(map(lambda x: len(x.split()), split_text)) == len(
+            list(filter(None, re.split(r'[:; |/\\]', text))))
+        assert abs(sum(map(len, split_text)) - len(text)) <= 1
+        assert split_text == expected_result
