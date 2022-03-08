135231170d794dde9218b53f8a79fb0878c82104
Florencia Cavallin <florencia.cavallin@ondewo.com>
2022-03-08 16:03:30 +0100

    [feature/OND232-320] uncommit changes of test normalizer for english

--- tests/unit/test_normalize_text_en.py
+++ tests/unit/test_normalize_text_en.py
@@ -176,12 +176,12 @@ class TestNormalization:
         # Test Times Normalization
         ('text 01:20 text', 'text one twenty text'),
         ('text 01:20:00 text', 'text one hour twenty minutes and zero seconds text'),
-        ('text 30:50 text', 'text thirty : fifty text'),
-        ('text 30:50:00 text', 'text thirty : fifty : zero zero text'),
-        ('text 25:40 text', 'text twenty five : forty text'),
+        ('text 30:50 text', 'text 30:50 text'),
+        ('text 30:50:00 text', 'text 30:50:00 text'),
+        ('text 25:40 text', 'text 25:40 text'),
         ('text 23:40 text', 'text twenty three forty text'),
         ('text 1:40 text', 'text one forty text'),
-        ('text 1:4 text', 'text one : four text'),
+        ('text 1:4 text', 'text 1:4 text'),
 
         # Test Ordinals Normalization
         ('1st', 'first'),
@@ -195,9 +195,9 @@ class TestNormalization:
         ('2.0', 'two point zero'),
 
         # Test Currencies Normalization
-        ('$0.12', 'twelve cents'),
+        ('$0.12', 'zero point one two dollars'),
         ('€1.123322', 'one point one two three three two two euros'),
-        ('£2.0', 'two pounds'),
+        ('£2.0', 'two point zero pounds'),
 
         # Test Telephones Normalization
         ('+1 (632) 401-5130', 'one, six three two, four o one, five one three o'),
@@ -211,7 +211,7 @@ class TestNormalization:
         ('1000km', 'one thousand kilometers'),
 
         # Test Date Normalization
-        (str(date(day=15, month=3, year=1)), 'zero zero zero one-zero three-one five'),  # does not understand
+        (str(date(day=15, month=3, year=1)), 'zero zero zero one zero three one five'),  # does not understand
         # 0001-03-15 as a date, probably because of the year
         (str(date(day=5, month=5, year=2020)), 'may fifth twenty twenty'),  # by default, dates are in lower case
 
@@ -222,16 +222,16 @@ class TestNormalization:
         ('065789', 'zero six five seven eight nine'),
 
         # Test Split Numbers Normalization
-        ('ggg 165789lkkkk', 'ggg one six five seven eight nine lkkkk'),  # its a code
-        ('ggg165789lkkkk', 'ggg one six five seven eight nine lkkkk'),
-        ('ggg 165789lkkkk', 'ggg one six five seven eight nine lkkkk'),
+        ('ggg 165789lkkkk', 'ggg one six five seven eight nine l k k k k'),  # its a code
+        ('ggg165789lkkkk', 'g g g one six five seven eight nine l k k k k'),
+        ('ggg 165789lkkkk', 'ggg one six five seven eight nine l k k k k'),
 
         # Test Dates Normalization
         ('1. January 2018  12. January',
          'one . january twenty eighteen twelve . January'),  # not a valid format
         ('What did you do on April 3rd 1989?',
-         'What did you do on april third nineteen eighty nine ?'),
-        ('12-01', 'twelve - zero one'),  # its a code
+         'What did you do on April third nineteen eighty nine ?'),
+        ('12-01', 'one two zero one'),  # its a code
         ('12.', 'twelve .'),
 
         # Test Urls Normalization
@@ -241,28 +241,25 @@ class TestNormalization:
         # Test Split and Normalize
         ('still need your date of birth. '
          'For example, say February 25 1989.',
-         'still need your date of birth . For example , say february twenty fifth '
-         'nineteen eighty nine .'),
-        ('January 1st 1989. Is that correct?', 'january first nineteen eighty nine . Is that correct ?'),
-        ('first of January. Is that correct?', 'first of January . Is that correct ?'),
-        ("You were born on 12-26-1944. Right?", 'You were born on december twenty sixth nineteen forty four . Right ?'),
+         'still need your date of birth. For example, say february twenty fifth nineteen eighty nine .'),
+        ('January 1st 1989. Is that correct?', 'January first nineteen eighty nine . Is that correct?'),
+        ('first of January. Is that correct?', 'first of January. Is that correct?'),
+        ("You were born on 12-26-1944. Right?", 'You were born on december twenty sixth nineteen forty four . Right?'),
         (
-            "You were born on 12-22-1944. Right? ",
-            'You were born on december twenty second nineteen forty four . Right ?'),
+            "You were born on 12-22-1944. Right? ", 'You were born on december twenty second nineteen forty four . Right?'),
         ("my daughter's grandfather has to make appointments on 10-05-1936",
-         "my daughter ' s grandfather has to make appointments on october fifth "
-         'nineteen thirty six'),
+         "my daughter's grandfather has to make appointments on october fifth nineteen thirty six"),
         ("my boy have to book another child appointment for 08-26-2027",
          'my boy have to book another child appointment for august twenty sixth twenty twenty seven'),
         ("I would like to change my boy s appointment for 03-09-1998",
          "I would like to change my boy s appointment for march ninth nineteen ninety eight"),
         ('she was born on January 15th 1998',
-         'she was born on january fifteenth nineteen ninety eight'),
+         'she was born on January fifteenth nineteen ninety eight'),
         ("Your social security number is 1234 and you were born on 15. January 1998. Right?",
-         'Your social security number is twelve thirty four and you were born on '
-         'fifteen . january nineteen ninety eight . Right ?'),
-        ("How are you???", 'How are you ? ? ?'),
-        ('text 30:50:00 text', 'text thirty : fifty : zero zero text')
+         "Your social security number is twelve thirty four and you were born on fifteen . january nineteen ninety "
+         "eight . Right?"),
+        ("How are you???", "How are you???"),
+        ('text 30:50:00 text', 'text 30:50:00 text')
     ])
     def test_nemo_normalize(text: str, expected_result: str) -> None:
         normalized_text: str = normalizer.normalize_nemo(text)
